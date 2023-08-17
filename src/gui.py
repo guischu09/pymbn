51aaf34d723b3b18884cfcf12dc48ccfe6932c97
Guilherme Schu <guischu09@gmail.com>
2023-08-16 22:22:20 -0300

    gui is finished. Still needs integration with main

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -20,7 +21,7 @@ class InputDialog(QDialog):
 
         self.setWindowTitle("Settings")
 
-        self.title1_label = QLabel("MS-scheme parameters")
+        self.title1_label = QLabel("MS-scheme parameters:")
         self.title1_label.setObjectName("titleLabel")
         self.title1_label.setStyleSheet("font-size: 14px; font-weight: bold; margin-bottom: 10px;")
 
@@ -71,8 +72,10 @@ class InputDialog(QDialog):
         self.out_format_label = QLabel("Output format:")
         self.out_format_combo = QComboBox()
         self.out_format_combo.setObjectName("out_formatCombo")
-        self.out_format_combo.addItem("human")
-        self.out_format_combo.addItem("rat/mouse")
+        self.out_format_combo.addItem("png")
+        self.out_format_combo.addItem("jpg")
+        self.out_format_combo.addItem("svg")
+        self.out_format_combo.addItem("pdf")
 
         self.name_label = QLabel("Name:")
         self.name_input = QLineEdit()
@@ -110,29 +113,55 @@ class InputDialog(QDialog):
         self.nsamples_input = QLineEdit()
         self.nsamples_input.setObjectName("nsamplesInput")
         self.nsamples_input.setText("2000")  # Set default value
-        self.nsamples_slider = QSlider()
-        self.nsamples_slider.setOrientation(1)  # Vertical slider
-        self.nsamples_slider.setRange(10, 10000)  # Range from 0 to 1, scaled by 100
-        self.nsamples_slider.setValue(30)  # Default value scaled by 100
 
         self.error_label = QLabel("")
         self.error_label.setObjectName("errorLabel")
         self.error_label.setStyleSheet("color: red;")
 
-        self.checkbox_label = QLabel("Some text:")
-        self.checkbox = QCheckBox()
-        self.checkbox.setObjectName("checkBox")
-        self.checkbox.setChecked(True)  # Set initial state to checked
+        self.n_measures_label = QLabel("Number of measures:")
+        self.n_measures = QCheckBox()
+        self.n_measures.setObjectName("n_measures")
+        self.n_measures.setChecked(True)  # Set initial state to checked
 
-        self.checkbox_input = QLineEdit()
-        self.checkbox_input.setObjectName("checkBoxInput")
-        self.checkbox_input.setEnabled(True)  # Enable input by default
+        self.n_measures_input = QLineEdit()
+        self.n_measures_input.setObjectName("n_measuresInput")
+        self.n_measures_input.setEnabled(True)  # Enable input by default
+        self.n_measures_input.setText("30")  # Set default value
 
-        self.checkbox.stateChanged.connect(self.update_input_state)
+        self.n_measures.stateChanged.connect(self.update_input_state)
 
-        self.plotting_options_label = QLabel("Plotting options")
+        self.plotting_options_label = QLabel("Plotting options:")
         self.plotting_options_label.setObjectName("plottingOptionsLabel")
         self.plotting_options_label.setStyleSheet("font-size: 14px; font-weight: bold; margin-top: 20px;")
+
+        self.brain_3d_label = QLabel("Plot 3D brain")
+        self.brain_3d = QCheckBox()
+        self.brain_3d.setObjectName("brain_3d")
+        self.brain_3d.setChecked(False)  # Set initial state to checked
+
+        self.interactive_3d_label = QLabel("Interactive 3D brain")
+        self.interactive_3d = QCheckBox()
+        self.interactive_3d.setObjectName("interactive_3d")
+        self.interactive_3d.setChecked(False)  # Set initial state to checked
+        self.interactive_3d.setEnabled(False)
+
+        self.brain_3d.stateChanged.connect(self.update_interactive_3d_state)
+
+        self.space_label = QLabel("")
+        self.space_label.setObjectName("plottingOptionsLabel")
+        self.space_label.setStyleSheet("font-size: 10px")
+
+        self.heatmap_label = QLabel("Plot heatmap")
+        self.heatmap = QCheckBox()
+        self.heatmap.setObjectName("heatmap")
+        self.heatmap.setChecked(True)  # Set initial state to checked
+        self.heatmap.setEnabled(False)
+
+        self.circle_label = QLabel("Plot circle")
+        self.circle = QCheckBox()
+        self.circle.setObjectName("circle")
+        self.circle.setChecked(True)  # Set initial state to checked
+        self.circle.setEnabled(True)
 
         self.submit_button = QPushButton("Submit")
         self.submit_button.setObjectName("submitButton")
@@ -168,11 +197,11 @@ class InputDialog(QDialog):
 
         brain_type_layout = QHBoxLayout()
         brain_type_layout.addWidget(self.brain_type_label)
-        brain_type_layout.addWidget(self.data_balance_combo)
+        brain_type_layout.addWidget(self.brain_type_combo)
 
         out_format_layout = QHBoxLayout()
         out_format_layout.addWidget(self.out_format_label)
-        out_format_layout.addWidget(self.data_balance_combo)
+        out_format_layout.addWidget(self.out_format_combo)
 
         name_layout = QHBoxLayout()
         name_layout.addWidget(self.name_label)
@@ -196,12 +225,30 @@ class InputDialog(QDialog):
         nsamples_layout = QHBoxLayout()
         nsamples_layout.addWidget(self.nsamples_label)
         nsamples_layout.addWidget(self.nsamples_input)
-        nsamples_layout.addWidget(self.nsamples_slider)
 
-        checkbox_layout = QHBoxLayout()
-        checkbox_layout.addWidget(self.checkbox)
-        checkbox_layout.addWidget(self.checkbox_label)
-        checkbox_layout.addWidget(self.checkbox_input)
+        n_measures_layout = QHBoxLayout()
+        n_measures_layout.addWidget(self.n_measures)
+        n_measures_layout.addWidget(self.n_measures_label)
+        n_measures_layout.addWidget(self.n_measures_input)
+
+        brain_3d_layout = QHBoxLayout()
+        brain_3d_layout.addWidget(self.brain_3d)
+        brain_3d_layout.addWidget(self.brain_3d_label)
+
+        interactive_3d_layout = QHBoxLayout()
+        interactive_3d_layout.addWidget(self.interactive_3d)
+        interactive_3d_layout.addWidget(self.interactive_3d_label)
+
+        heatmap_layout = QHBoxLayout()
+        heatmap_layout.addWidget(self.heatmap)
+        heatmap_layout.addWidget(self.heatmap_label)
+
+        circle_layout = QHBoxLayout()
+        circle_layout.addWidget(self.circle)
+        circle_layout.addWidget(self.circle_label)
+
+        space_layout = QHBoxLayout()
+        space_layout.addWidget(self.space_label)
 
         # Layouts order
         layout.addLayout(method_layout)
@@ -212,14 +259,18 @@ class InputDialog(QDialog):
         layout.addLayout(correction_layout)
         layout.addLayout(sampling_layout)
         layout.addLayout(nsamples_layout)
-
         layout.addLayout(representation_layout)
         layout.addLayout(data_balance_layout)
-
-        layout.addLayout(name_layout)
-        layout.addLayout(checkbox_layout)
-
+        layout.addLayout(n_measures_layout)
         layout.addWidget(self.plotting_options_label)
+        layout.addWidget(self.space_label)
+        layout.addLayout(brain_3d_layout)
+        layout.addLayout(interactive_3d_layout)
+        layout.addLayout(brain_type_layout)
+        layout.addLayout(space_layout)
+        layout.addLayout(heatmap_layout)
+        layout.addLayout(circle_layout)
+        layout.addLayout(out_format_layout)
 
         layout.addWidget(self.error_label)
 
@@ -228,62 +279,70 @@ class InputDialog(QDialog):
         self.setLayout(layout)
 
         self.alpha_slider.valueChanged.connect(
-            lambda value: self.update_slider_value(value, self.alpha_input, self.alpha_slider)
+            lambda value: self.update_slider_value(value, self.alpha_input, self.alpha_slider, 0, 1)
         )
         self.alpha_input.textChanged.connect(
-            lambda text: self.update_slider_from_input(text, self.alpha_input, self.alpha_slider)
+            lambda text: self.update_slider_from_input(text, self.alpha_input, self.alpha_slider, 0, 1)
         )
 
         self.theta_slider.valueChanged.connect(
-            lambda value: self.update_slider_value(value, self.theta_input, self.theta_slider)
+            lambda value: self.update_slider_value(value, self.theta_input, self.theta_slider, 0, 1)
         )
         self.theta_input.textChanged.connect(
-            lambda text: self.update_slider_from_input(text, self.theta_input, self.theta_slider)
+            lambda text: self.update_slider_from_input(text, self.theta_input, self.theta_slider, 0, 1)
         )
         self.threshold_slider.valueChanged.connect(
-            lambda value: self.update_slider_value(value, self.threshold_input, self.threshold_slider)
+            lambda value: self.update_slider_value(value, self.threshold_input, self.threshold_slider, 0, 1)
         )
         self.threshold_input.textChanged.connect(
-            lambda text: self.update_slider_from_input(text, self.threshold_input, self.threshold_slider)
+            lambda text: self.update_slider_from_input(text, self.threshold_input, self.threshold_slider, 0, 1)
         )
 
-        self.nsamples_slider.valueChanged.connect(
-            lambda value: self.update_slider_value(value, self.nsamples_input, self.nsamples_slider)
-        )
-        self.nsamples_input.textChanged.connect(
-            lambda text: self.update_slider_from_input(text, self.nsamples_input, self.nsamples_slider)
-        )
+    def update_interactive_3d_state(self, state):
+        if state == Qt.Checked:
+            self.interactive_3d.setEnabled(True)
+        else:
+            self.interactive_3d.setEnabled(False)
+            self.interactive_3d.setChecked(False)
 
     def update_input_state(self, state):
-        self.checkbox_input.setEnabled(state == 2)  # 2 corresponds to checked state
+        self.n_measures_input.setEnabled(state == 2)  # 2 corresponds to checked state
 
-    def update_slider_value(self, value, input_field, slider):
-        scaled_value = self.scale_slider_value(value)
+    def update_slider_value(self, value, input_field, slider, min_value, max_value):
+        scaled_value = self.scale_slider_value(value, min_value, max_value)
         input_field.setText(str(scaled_value))
 
-    def scale_slider_value(self, slider_value):
-        return self.min_value + (slider_value / 100.0) * (self.max_value - self.min_value)
+    def scale_slider_value(self, slider_value, min_value, max_value):
+        # if max_value > 1:
+        # return int(min_value + (slider_value / 100.0) * (max_value - min_value))
+        return min_value + (slider_value / 100.0) * (max_value - min_value)
 
-    def update_slider_from_input(self, text, input_field, slider):
+    def update_slider_from_input(self, text, input_field, slider, min_value, max_value):
         try:
             scaled_value = float(text)
-            if 0 <= scaled_value <= 1:  # Check valid range
+            if min_value <= scaled_value <= max_value:  # Check valid range
                 input_field.setStyleSheet("")  # Reset border color
-                slider_value = int(scaled_value * 100)
+                slider_value = self.inverse_scale_slider_value(scaled_value, min_value, max_value)
                 slider.setValue(slider_value)
                 self.error_label.setText("")  # Clear any error message
             else:
                 input_field.setStyleSheet("border: 1px solid red;")
-                self.error_label.setText(f"Invalid input: Value must be between 0 and 1 for {input_field.objectName()}")
+                self.error_label.setText(
+                    f"Invalid input: Value must be between {min_value} and {max_value} for {input_field.objectName()}"
+                )
         except ValueError:
             input_field.setStyleSheet("border: 1px solid red;")
-            self.error_label.setText(f"Invalid input: Not a valid number.")
+            self.error_label.setText(f"Invalid input: Not a valid number for {input_field.objectName()}")
+
+    def inverse_scale_slider_value(self, scaled_value, min_value, max_value):
+        return int((scaled_value - min_value) / (max_value - min_value) * 100)
 
     def submit_clicked(self):
         attribute_info = [
             ("alpha", self.alpha_input, self.alpha_slider),
             ("theta", self.theta_input, self.theta_slider),
             ("threshold", self.threshold_input, self.threshold_slider),
+            ("nsamples", self.nsamples_input, self.nsamples_slider),
         ]
         name = self.name_input.text()
         valid_input = True
@@ -309,8 +368,8 @@ class InputDialog(QDialog):
         if valid_input:
             self.name = name
             self.method = self.method_combo.currentText()
-            self.checkbox_state = self.checkbox.isChecked()
-            self.checkbox_text = self.checkbox_input.text() if self.checkbox_state else ""
+            self.n_measures_state = self.n_measures.isChecked()
+            self.n_measures_text = self.n_measures_input.text() if self.n_measures_state else ""
             self.accept()
 
 
